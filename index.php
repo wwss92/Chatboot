37efe13b2b4dbb2f75a79e2ec68b9c3cbb50b3ed
wwss92 <42390734+wwss92@users.noreply.github.com>
2020-06-03 16:22:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Hola mundo";
+$access_token = "EAANoKKNNwMMBAMZAYhiLR1h0g5tzxivsRZAlGznMrCkkEzh8Nv8ZAOk6NLIoz6f6WIE14gfvyPWd86uLTr53j47bl5wAemx7SqSZBeYvi3TemORATgX0XGrhiPHtrANNk3Au1W6qAfTiP37ktp4SKzpYlFKHiM2K4QXa3tgMt2hrRV5Ia0ctysIZBTzvto8UZD";
+$verify_token = "fb_time_bot";
+$hub_verify_token = null;
+if(isset($_REQUEST['hub_challenge'])) {
+ $challenge = $_REQUEST['hub_challenge'];
+ $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+if ($hub_verify_token === $verify_token) {
+ echo $challenge;
+}
 
 ?>
