01257388534be7dfd0c731cd8b6f59683e8c3f3a
wwss92 <42390734+wwss92@users.noreply.github.com>
2020-06-03 16:54:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,59 @@
 <?php
 
-// your access token generated from the facebook developer console
-$access_token = 'EAANoKKNNwMMBAMZAYhiLR1h0g5tzxivsRZAlGznMrCkkEzh8Nv8ZAOk6NLIoz6f6WIE14gfvyPWd86uLTr53j47bl5wAemx7SqSZBeYvi3TemORATgX0XGrhiPHtrANNk3Au1W6qAfTiP37ktp4SKzpYlFKHiM2K4QXa3tgMt2hrRV5Ia0ctysIZBTzvto8UZD';
+/**
+ * Webhook for Time Bot- Facebook Messenger Bot
+ */
 
-// the json input sent from facebook
+include 'config.php';
+$hub_verify_token = null;
+
+if(isset($_REQUEST['hub_challenge'])) {
+    $challenge = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+
+
+if ($hub_verify_token === $verify_token) {
+    echo $challenge;
+}
+
 $input = json_decode(file_get_contents('php://input'), true);
 
-// the URL to send messages back to the BOT
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-
-/**** THIS IS THE AUTHENTICATION ***/
-$hub_verify_token = null;
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-
-if ($verify_token === 'fb_time_bot') {
-  echo $challenge;
-}
-/******** THIS IS THE END OF THE AUTHENTICATION ***/
-
-// gets the sender and message sent
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-// mark the message that was sent to the BOT as seen
-$markAsSeen = curl_init($url);
-$jsonData = '{
-    "recipient":{
-        "id":"'.$sender.'"
-    },
-    "sender_action":"mark_seen"
-}';
-curl_setopt($markAsSeen, CURLOPT_POST, 1);
-curl_setopt($markAsSeen, CURLOPT_POSTFIELDS, $jsonData);
-curl_setopt($markAsSeen, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-$result = curl_exec($markAsSeen);
+$message_to_reply = '';
 
-// get the information about the user
-$profile_url = 'https://graph.facebook.com/v2.6/'.$sender.'?access_token='.$access_token;
+/**
+ * Some Basic rules to validate incoming messages
+ */
+if(preg_match('[time|current time|now|время|час]', strtolower($message))) {
 
-// get the user profile and do what ever with this
-$profileInfo = json_decode(file_get_contents($profile_url));
+    $time = getdate();
+    $hours = $time['hours'];
+    if ($time['minutes']<10) {
+        $minutes = "0".$time['minutes'];
+    } else {
+        $minutes = $time['minutes'];
+    }
+    $response = $hours.":".$minutes;
+    if($response != '') {
+        $message_to_reply = $response;
+    } else {
+        $message_to_reply = "Sorry, I don't know.";
+    }
+} else {
+    $message_to_reply = 'Sorry, I don\'t understand you. I can only tell what time it is now.';
+}
 
-// send a message back to the user
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+
+
+//Initiate cURL.
 $ch = curl_init($url);
-$message_to_reply = 'You said: '.$message;
+
+//The JSON data.
 $jsonData = '{
     "recipient":{
         "id":"'.$sender.'"
@@ -53,12 +62,21 @@ $jsonData = '{
         "text":"'.$message_to_reply.'"
     }
 }';
+
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+
+//Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-// send this message only if the user sent us a message
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+
+//Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
 }
-
-?>
