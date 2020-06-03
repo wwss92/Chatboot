ed47339813ea7d413dae461d57c73eeb97090432
wwss92 <42390734+wwss92@users.noreply.github.com>
2020-06-03 16:55:58 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Your Page Access Token
+$access_token = "EAANoKKNNwMMBAMZAYhiLR1h0g5tzxivsRZAlGznMrCkkEzh8Nv8ZAOk6NLIoz6f6WIE14gfvyPWd86uLTr53j47bl5wAemx7SqSZBeYvi3TemORATgX0XGrhiPHtrANNk3Au1W6qAfTiP37ktp4SKzpYlFKHiM2K4QXa3tgMt2hrRV5Ia0ctysIZBTzvto8UZD";
+
+// Your webhook varification token
+$verify_token = "fb_time_bot";
+
+?>
